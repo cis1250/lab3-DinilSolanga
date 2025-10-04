a3b19603a514ef097d400404aebe76dbb0daf0f2
Dins <129677977+DinilSolanga@users.noreply.github.com>
2025-10-04 16:18:11 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
 terms = int(input("How many terms of the fibonacci sequence would you like"))
 # Validate that the input is a positive integer.
